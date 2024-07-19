c476ad9a804980f39abdb9d8b589900140d8a637
unknown <rouchashoori@gmail.com>
2024-07-19 19:17:34 +0330

    cleaned up some codes

--- cmd/start.go
+++ cmd/start.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"github.com/Rouch3362/roderfile/helpers"
+	"github.com/Rouch3362/roderfile/prompts"
 	"github.com/spf13/cobra"
 )
 
@@ -12,11 +13,42 @@ var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "a cli tool that you can organize you files with",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		_, err := helpers.ReadFiles(args[0])
-		return err
+		err := start()
+
+		if err != nil {
+			return err
+		}
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
+}
+
+
+func start() error {
+	dirPath, err := prompts.GetDirectoryPrompt()
+
+	if err != nil {
+		return err
+	}
+
+	r, err := helpers.ReadFiles(dirPath)
+	if err != nil {
+		return err
+	}
+	err = helpers.RemoveDuplicates(dirPath,r)
+
+	if err != nil {
+		return err
+	}
+
+	err = helpers.CategorizeFiles(r)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
