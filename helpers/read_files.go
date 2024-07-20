058c8b2ce9e69579b886872084f587964f85778b
unknown <rouchashoori@gmail.com>
2024-07-21 00:23:39 +0330

    added option to do or do not search deep in files and also error handling for when no file found

--- helpers/read_files.go
+++ helpers/read_files.go
@@ -1,14 +1,19 @@
 package helpers
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path"
 )
 
 // reads all files in a directory
-func ReadFiles(dirPath string) (*[]string,error){
-	
+func ReadFiles(dirPath string , deepSearch bool) (*[]string,error){
+
+	if !deepSearch {
+		YellowLog("⚠️  You Turned Deep Search Off")
+	}
+
 	GreenLog(fmt.Sprintf("📂 Fetching File(s) From %s", dirPath))
 	entries, err := os.ReadDir(dirPath)
 	
@@ -21,15 +26,27 @@ func ReadFiles(dirPath string) (*[]string,error){
 	for _,entry := range entries{
 		// check if the entry is a directory or a file
 		if entry.IsDir(){
-			// if it is a directory it will use recursion to read all files
-			newPath := path.Join(dirPath, entry.Name())
-			nestedFiles , _ := ReadFiles(newPath)
-			// append returned values from recursion call to files slice
-			files = append(files, *nestedFiles...)
+			// if deep search is on i will crawl
+			if deepSearch {
+				// if it is a directory it will use recursion to read all files
+				newPath := path.Join(dirPath, entry.Name())
+				nestedFiles , _ := ReadFiles(newPath, deepSearch)
+				// append returned values from recursion call to files slice
+				files = append(files, *nestedFiles...)
+			} else {
+				continue 
+			}
 		} else {
 			files = append(files, path.Join(dirPath,entry.Name()))
 		}
 	}
+
+	// if nothing founded
+	if len(files) < 1 {
+		RedLog("🔴 No Files Found")
+		return nil, errors.New("no files found")
+	}
+
 	GreenLog(fmt.Sprintf("✅ Got Files From %s" , dirPath))
 	return &files, nil
 }
